917336f4cb6abb3662666fbeb5a3be7a578bc687
史蒂夫 <steam114514@126.com>
2025-02-22 12:34:44 +0800

    Add files via upload

---
+++ ServerPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServerPlugin")]
+[assembly: AssemblyDescription("服务器通用轻插件")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("萌新社区开发团队")]
+[assembly: AssemblyProduct("ServerPlugin")]
+[assembly: AssemblyCopyright("© 萌新社区开发团队")]
+[assembly: AssemblyTrademark("萌新社区开发团队")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3ab9047b-6b73-4d0d-b969-b4eb545268fe")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
