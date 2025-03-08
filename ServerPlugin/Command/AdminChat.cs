95723524464bae29af4678f171f8b55ab2f118d1
史蒂夫 <steam114514@126.com>
2025-03-08 18:16:59 +0800

    fix : 无法丢弃物品

---
+++ ServerPlugin/Command/AdminChat.cs
@@ -0,0 +1,61 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using RemoteAdmin;
+using System;
+using System.Linq;
+
+namespace ServerPlugin.Command
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class AdminChat : ICommand,IUsageProvider
+    {
+        public string Command { get; } = "ac";
+        public string[] Aliases { get; } = new string[1] { "ac" };
+        public string Description { get; } = "呼叫管理员(不要使用该指令骚扰管理员)";
+        public string[] Usage { get; } = new string[] { "*文本" };
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (sender is PlayerCommandSender playerCommandSender)
+            {
+                int AdminCount = Player.List.Where(x => x.RemoteAdminAccess).ToList().Count;
+                Player player = Player.Get(playerCommandSender.PlayerId);
+                if (player.IsVerified && player != null)
+                {
+                    if (arguments.Count > 0)
+                    {
+                        if (player.RemoteAdminAccess)
+                        {
+                            response = "<color=red>管理不能呼叫管理！</color>";
+                            return true;
+                        }
+                        if (AdminCount != 0)
+                        {
+                            foreach (Player AdminPlayer in Player.List.Where(x => x.RemoteAdminAccess))
+                                AdminPlayer.Broadcast(5, $"<size=55%><color=#00FFFF>[管理呼叫]</color> {player.Nickname} : {string.Join(" ", arguments)}</size>");
+                            Log.Info($"[Plugin-Call]-[呼叫][{player.Nickname}][{player.UserId}]:[{string.Join(" ", arguments)}]");
+                        }
+                        else
+                        {
+                            Log.Info($"[Plugin-Call]-[未响应呼叫][{player.Nickname}][{player.UserId}]:[{string.Join(" ", arguments)}]");
+                            response = "<color=red>服务器暂无管理员!</color>";
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        response = "<color=red>请输入文本!</color>";
+                        return false;
+                    }
+                }
+                else
+                {
+                    response = "<color=red>你还没加入服务器！</color>";
+                    return false;
+                }
+
+            }
+            response = "<color=green>信息已发送!</color>";
+            return true;
+        }
+    }
+}
---
+++ ServerPlugin/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Features;
+
+namespace ServerPlugin
+{
+    public class Plugin : Plugin<Config>
+    {
+        public const string Package = "caixukun.steve.serverplugin";
+        public static Plugin Ins { get; } = new Plugin();
+        public static string PluginVersion { get; } = "1.0.1";
+        public override string Author { get; } = "萌新社区开发团队";
+        public override string Name { get; } = "ServerPlugin";
+        public EventHandlers EventHandlers { get; } = new EventHandlers();
+        public override void OnEnabled() => EventHandlers.RegEvent();
+        public override void OnDisabled() => EventHandlers.UnRegEvent();
+    }
+}
